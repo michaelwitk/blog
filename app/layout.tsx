29f279c63724579e112e562b2781fe8ac4ce38e3
michaelwitk <mwit2023@gmail.com>
2025-03-14 01:18:36 +0100

    minimal

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,18 @@ export default function RootLayout({
         >
             <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
                 <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-                    <Navbar />
+                    {/* <Navbar /> */}
+                    <div>
+                        <p className="mb-4">
+                            <a
+                                href="https://x.com/michael_witk"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                @michael_witk on X (Twitter)
+                            </a>
+                        </p>
+                    </div>
                     {children}
                     <Footer />
                 </main>
--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,10 @@ import { BlogPosts } from 'app/components/posts'
 export default function Page() {
     return (
         <section>
-            <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+            {/* <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
                 My Blog
-            </h1>
-            <p className="mb-4">
-                <a
-                    href="https://x.com/michael_witk"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    @michael_witk on X (Twitter)
-                </a>
-            </p>
-            <div className="my-8">
+            </h1> */}
+            <div className="my-8 min-h-screen">
                 <BlogPosts />
             </div>
         </section>
