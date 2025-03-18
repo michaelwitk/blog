a3643f52f705ed200a40250735024a240febdd44
michaelwitk <mwit2023@gmail.com>
2025-03-19 00:14:00 +0100

    env

--- app/api/route.js
+++
@@ -1,7 +0,0 @@
-export const GET = async () => {
-    console.log('debug', new Date(), process.id)
-    return Response.json({
-        NEXT_PUBLIC_MESSAGE: process.env.NEXT_PUBLIC_MESSAGE,
-        pid: process.pid,
-    })
-}
